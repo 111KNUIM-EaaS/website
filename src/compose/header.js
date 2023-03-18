3dfab78e5ca4c626e35c9f98e22913a39a085193
Reaxoh <64882473+Reaxoh@users.noreply.github.com>
2023-03-18 02:21:40 +0000

    :memo:feat: update header content
    This commit updates the content of the header on the page.

--- src/compose/header.js
+++ src/compose/header.js
@@ -1,6 +1,45 @@
-const Header = () => { 
-    return(
-        <div>This is Header</div>
+import { Form, Nav, Navbar } from "react-bootstrap"
+import { List, QuestionCircle, Grid3x3GapFill, PersonFill} from "react-bootstrap-icons"
+
+import 'bootstrap/dist/css/bootstrap.min.css';;
+
+let icons =  [
+    {id: "QuestionCircle", svg: <QuestionCircle size={30}/>, href: "/information"},
+    {id: "Grid3x3GapFill", svg: <Grid3x3GapFill size={30}/>, href: "/content"}
+]
+
+function Header() {    
+    return (
+        <Navbar style={{ height: "10vh", backgroundColor: 'rgb(238, 242, 249)' }}>
+            <Nav className="p-4 d-none d-md-block">
+                <List size={30}/>
+            </Nav>
+            <Navbar.Brand className="px-3">
+                <a href="/content">
+                    <img
+                        alt="Eaas"
+                        src=''
+                        height={35}
+                    />
+                </a>
+            </Navbar.Brand>
+            <Form className="d-flex d-none d-md-block ps-5">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-5"
+                    aria-label="Search"
+                  />
+            </Form> 
+            <Nav className="ms-auto pe-4">
+                {icons.map((icon,index) => (
+                    <Nav.Link key={index} href={icon.href}>
+                        {icon.svg}
+                    </Nav.Link>
+                ))}
+            </Nav>
+        </Navbar>
     )
 }
+
 export default Header
