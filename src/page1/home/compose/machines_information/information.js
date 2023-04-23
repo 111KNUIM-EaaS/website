b276e9d98df2cf4c60c1dbcf8222ca1436af0cd0
Hsun <hsun1031@outlook.com>
2023-04-23 06:29:12 +0000

    ✨ feat: Add website can update OTA data
    Now website can update OTA data, and it will be saved in database.

--- src/page1/home/compose/machines_information/information.js
+++ src/page1/home/compose/machines_information/information.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Container, Row, Col, Nav, Navbar, Toast, Form, Button} from "react-bootstrap"
-import { PlayBtn, PauseBtn, StopBtn, Hammer } from "react-bootstrap-icons";
+import ReactMarkdown from 'react-markdown';
+import { Card, Container, Row, Col, Nav, Navbar, Toast, Form, Button, DropdownButton, ButtonGroup, Dropdown, ListGroup} from "react-bootstrap"
+import { PlayBtn, PauseBtn, StopBtn, Hammer, Tag, FileEarmarkMedical } from "react-bootstrap-icons";
 import { authentication } from "../../../../compose/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,12 +10,16 @@ import axios from 'axios';
 import apiConf from '../../../../conf/apiConf.json'
 
 const MachineInformation = () => {
-    const [validated, setValidated] = useState(false);
-    const [user, setUser] = useState(null);
-    const [showA, setShowA] = useState(false);
-    const [rid, setRid] = useState(undefined);
-    const toggleShowA = () => setShowA(!showA);
+    const [rid,                setRid               ] = useState(undefined);
+    const [user,               setUser              ] = useState(null);
+    const [showA,              setShowA             ] = useState(false);
+    const [validated,          setValidated         ] = useState(false);
+    const [machineInfo,        setMachineInfo       ] = useState(undefined);
+    const [gitHubOTAData,      setGitHubOTAData     ] = useState(undefined);
+    const [githubReleasesList, setGithubReleasesList] = useState([]);
 
+    const toggleShowA = () => setShowA(!showA);
+    
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
         const value  = params.get('rid');
@@ -42,12 +47,35 @@ const MachineInformation = () => {
         // axios
         axios.post(`http://${apiConf.host}:${apiConf.port}/api/machines/info`, data, { headers: headers })
              .then(res => {
-                console.log("🚀 ~ file: information.js:21 ~ useEffect ~ res data:", res.data);
+                const data = res.data;
+                console.log("🚀 ~ file: information.js:51 ~ getMachineInfo ~ data:", data)
+                setMachineInfo(data)
+                getGitHubOTAData(data.github.owner, data.github.repo);
              })
              .catch(err => {
                 console.log("information.js getMachineInfo err:", err);
-                window.location.href = "/home/state";
+                // window.location.href = "/home/state";
              });
+    }
+
+    const getGitHubOTAData = async (owner, repo) => {
+        try {
+            const response = 
+                await fetch(`https://api.github.com/repos/${owner}/${repo}/releases`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/vnd.github+json'
+                    },
+                });
+
+            const data = await response.json();
+            console.log("gitHubOTAData:", data);
+            setGithubReleasesList(data);
+            setGitHubOTAData(data[0]);
+        }
+        catch (error) {
+            console.log(`[https://api.github.com/repos/${owner}/${repo}/releases]error:`, error);
+        }
     }
 
     const icon_list = [
@@ -74,6 +102,41 @@ const MachineInformation = () => {
         setValidated(true);
     };
 
+    const handleSelect = (eventKey) => {
+        console.log('Selected event key:', eventKey);
+        setGitHubOTAData(githubReleasesList.find(releases => releases.tag_name === eventKey))
+    };
+
+    function handleButtonClick(type, file) {
+        // header
+        const headers = {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'User': user.uid,
+        };
+
+        // data
+        const data = {
+            rid: rid,
+            firmware: {
+                name: file.name,
+                url: file.browser_download_url,
+                tag: gitHubOTAData.tag_name,
+            }
+        }
+
+        // axios
+        axios.post(`http://${apiConf.host}:${apiConf.port}/api/machines/ota`, data, { headers: headers })
+             .then(res => {
+                console.log("information.js getMachineInfo res:", res.data);
+
+             })
+             .catch(err => {
+                console.log("information.js getMachineInfo err:", err);
+                // window.location.reload();
+             });
+    }
+
     return (
         <Container fluid style={{ position: 'relative', height: "95vh" }}>
             <Row className="pt-4" style={{ height: "15vh"}} >
@@ -93,7 +156,101 @@ const MachineInformation = () => {
             <Row style={{ height: "80vh"}}>
                 <Col>
                     <Card>
-                        this is Content
+                        <Container>
+                            <Row xs={1} md={1} className="align-items-center" style={{height: '85vh'}} >
+                                <Col>
+                                    <Card style={{height: '80vh'}} >
+                                        <Card.Header>
+                                            <Row>
+                                                <Col>
+                                                    <h2>{gitHubOTAData === undefined? "OTA 雲端更新" : gitHubOTAData.name}</h2>
+                                                </Col>
+                                                <Col style={{textAlign: 'right'}}>
+                                                    <DropdownButton as={ButtonGroup} title={gitHubOTAData === undefined? "Tag" :gitHubOTAData.tag_name} onSelect={handleSelect}>
+                                                        {
+                                                            githubReleasesList.map(releases => {
+                                                                
+                                                                return (
+                                                                    <Dropdown.Item key={releases.tag_name} eventKey={releases.tag_name}>{releases.tag_name}</Dropdown.Item>
+                                                                )
+                                                            })
+                                                        }
+                                                    </DropdownButton>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    <img
+                                                        src={gitHubOTAData === undefined? "" : `https://github.com/${gitHubOTAData.author.login}.png`}
+                                                        height="20"
+                                                        className="d-inline-block rounded-circle"
+                                                        alt={gitHubOTAData === undefined? "" : gitHubOTAData.author.login}
+                                                    />
+                                                    {gitHubOTAData === undefined? "" : ` ${gitHubOTAData.author.login}`} released this 
+                                                    {gitHubOTAData === undefined? "" : " " + (new Date(gitHubOTAData.published_at)).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}
+                                                </Col>
+                                                <Col>
+                                                    <Tag size={20} /> {gitHubOTAData === undefined? "" : gitHubOTAData.tag_name} 
+                                                </Col>
+                                            </Row>
+                                        </Card.Header>
+
+                                        <Card.Body>
+                                            <ReactMarkdown>{gitHubOTAData === undefined? "" : gitHubOTAData.body}</ReactMarkdown>
+                                        </Card.Body>
+
+                                        <Card.Footer>
+                                            <Card>
+                                                <Card.Header>
+                                                    Assets
+                                                </Card.Header>
+                                                <Card.Body>
+                                                    <ListGroup className="list-group-flush">
+                                                        {
+                                                            gitHubOTAData === undefined? ('No Assets') : gitHubOTAData.assets.map((file) => {
+                                                                if(!(/\.bin$/.test(file.name))) {
+                                                                    return null;
+                                                                }
+                                                                return(
+                                                                    <ListGroup.Item key={file.name}>
+                                                                        <Row className='align-items-center'>
+                                                                            <Col className='h3'>
+                                                                                <a href={file.browser_download_url}><FileEarmarkMedical size={30}/>{file.name}</a>
+                                                                            </Col>
+                                                                            <Col>
+                                                                                {(new Date(file.updated_at)).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}
+                                                                            </Col>
+                                                                            <Col className="d-grid gap-2" style={{textAlign: "right"}}>
+                                                                                {
+                                                                                    file.name.toLowerCase().indexOf("spiffs") !== -1 ? (
+                                                                                        <Button variant="outline-warning" size="lg" onClick={() => handleButtonClick("spiffs", file)}>
+                                                                                            燒入 {file.name}
+                                                                                        </Button>
+                                                                                    ) : file.name.toLowerCase().indexOf("firmware") !== -1 ? (
+                                                                                        <Button variant="outline-danger" size="lg" onClick={() => handleButtonClick("firmware", file)}>
+                                                                                            燒入 {file.name}
+                                                                                        </Button>
+                                                                                    ) : (
+                                                                                        <ButtonGroup size="lg">
+                                                                                            <Button variant="outline-primary" onClick={() => handleButtonClick("firmware", file)}>Firmware</Button>
+                                                                                            <Button variant="outline-primary" onClick={() => handleButtonClick("spiffs", file)}>SPIFFS</Button>
+                                                                                        </ButtonGroup>
+                                                                                    )
+                                                                                }
+                                                                            </Col>
+                                                                        </Row>
+                                                                    </ListGroup.Item>
+                                                                );
+                                                            })
+                                                        }
+                                                    </ListGroup>
+                                                </Card.Body>
+                                            </Card>
+                                        </Card.Footer>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
                     </Card>
                 </Col>
             </Row>
